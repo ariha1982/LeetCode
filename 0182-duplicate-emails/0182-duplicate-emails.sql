4d2994ab6a769e33ccf1c091c84fac8c1291007f
Jayde1012 <ariha1982@naver.com>
2025-05-13 18:12:14 +0900

    Time: 383 ms (64.34%), Space: 0B (100%) - LeetHub

--- 0182-duplicate-emails/0182-duplicate-emails.sql
+++ 0182-duplicate-emails/0182-duplicate-emails.sql
@@ -1,11 +1,6 @@
 # Write your MySQL query statement below
 select
-    e_cnt.email Email
-from (
-    select
-        email,
-        count(*) cnt
-    from Person
-    group by email
-) e_cnt
-where e_cnt.cnt > 1
+    email Email
+from Person
+group by email
+having count(*) > 1;
