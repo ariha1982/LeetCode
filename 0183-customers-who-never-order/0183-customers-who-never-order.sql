196c775eebd00cfd28418811ca1e4df792856617
Jayde1012 <ariha1982@naver.com>
2025-05-20 11:11:05 +0900

    Time: 590 ms (44.82%), Space: 0B (100%) - LeetHub

---
+++ 0183-customers-who-never-order/0183-customers-who-never-order.sql
@@ -0,0 +1,11 @@
+# Write your MySQL query statement below
+select
+    name Customers
+from Customers
+where id not in (
+    select
+        c.id
+    from Customers c
+    join Orders o
+        on c.id = o.customerId
+);
